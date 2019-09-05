530b825d3fb0ae9da6024055a139c8d6ebf4cbb9
unknown <diogo.zmm@gmail.com>
2019-09-05 14:49:37 -0300

    First Controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales web Application for C# Course.";
+            ViewData["Aluno"] = "Diogo Zimmermann";
 
             return View();
         }
